eadfe5d925cdcbe1bfa1c798c29d8c61f354089d
EricHuey28 <erichuey2828@gmail.com>
2025-12-03 12:54:07 -0800

    M9L2

---
+++ character_body_2d.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+var max_speed := 600.0
+
+
+func _process(delta: float) -> void:
+	var direction := Vector2(0, 0)
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+
+	velocity = direction * max_speed
+	position += velocity * delta
